6afa4fde796d8ac146e82c11727528cc1a123c61
Choupa-web <persan78@hotmail.com>
2021-06-22 09:02:31 +0200

    general UI structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,35 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { SidebarComponent } from './components/shared/sidebar/sidebar.component';
+import { AsideComponent } from './components/shared/aside/aside.component';
+import { HomeComponent } from './components/home/home.component';
+import { AuthModule } from '@auth0/auth0-angular';
+import { LoginButtonComponent } from './components/shared/login-button/login-button.component';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent,
+    AsideComponent,
+    HomeComponent,
+    LoginButtonComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AuthModule.forRoot({
+      domain: 'YOUR_DOMAIN',
+      clientId: 'YOUR_CLIENT_ID'
+    }),
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
