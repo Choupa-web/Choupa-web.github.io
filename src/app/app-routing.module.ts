215aafa94b24053d39c38250cee1f07028fc2074
sraye <sylvie.raye@budget-box.com>
2021-11-22 15:46:46 +0100

    SPOR-22 Gestion access des utilisateurs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { AuthGuard } from '@auth0/auth0-angular';
+import {DashboardComponent} from './components/dashboard/dashboard.component';
 
 
 const routes: Routes = [
-  {path: 'home', component: HomeComponent},
-  {path: '', component: HomeComponent},
-  {path: '**', component: HomeComponent }
+  { path: 'home', component: HomeComponent},
+  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
+  { path: '', component: HomeComponent},
+  { path: '**', component: HomeComponent }
 ];
 
 @NgModule({
