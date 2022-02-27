d3d91650ded064e4a7533786887f9f46333a6385
sraye <sylvie.raye@budget-box.com>
2022-02-27 07:03:26 +0100

    Gp-4_WS_fonction_ajout_d'une_activité

--- src/app/services/activities.service.ts
+++ src/app/services/activities.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
 import {Observable, of} from 'rxjs';
 import {HttpClient} from '@angular/common/http';
-import {AngularFirestore, DocumentReference} from '@angular/fire/compat/firestore';
+import {AngularFirestore, DocumentReference, QuerySnapshot} from '@angular/fire/compat/firestore';
 import {BaseActivityDatas, HometrainerActivityDatas} from '../models/activities.model';
 import {FirebaseCollections} from '../enums/activity.enum';
+import {getDocs, query, where} from '@angular/fire/firestore';
 
 
 
@@ -14,33 +15,30 @@ export class ActivitiesService {
 
   constructor(private http: HttpClient, private store: AngularFirestore) { }
 
+  getAllActivities(): Observable<any> {
+    return this.store.collection(FirebaseCollections.BASE_ACTIVITY).snapshotChanges();
+  }
 
- addBaseActivity(activity: BaseActivityDatas): Promise<DocumentReference<unknown>> {
+  addBaseActivity(activity: BaseActivityDatas): Promise<DocumentReference<unknown>> {
     return this.store.collection(FirebaseCollections.BASE_ACTIVITY).add(activity);
   }
 
   addHomeTrainerActivity(activity: HometrainerActivityDatas): Promise<DocumentReference<unknown>> {
     return this.store.collection(FirebaseCollections.HOMETRAINER_ACTIVITY).add(activity);
   }
-  getAllActivities(): Observable<any> {
-    return this.store.collection('activities').snapshotChanges();
+
+  deleteBaseActivity(id: string): Promise<void> {
+    return this.store.collection(FirebaseCollections.BASE_ACTIVITY).doc(id).delete();
   }
 
-  deleteActivity(id: number): Observable<boolean> {
+  deleteHometrainerActivity(id: string): Observable<boolean> {
     return of(true);
   }
 
- /* editActivity(activityToUpdate: HometrainerActivityDatas): Observable<HometrainerActivityDatas> {
-    return of(
-      {
-        id: '5',
-        averagePower: 107,
-        maxPower: 210,
-        ftp: 117,
-        energy: 50,
-        averageCadence: 80,
-        maxCadence: 110
-      }
-    );
-  }*/
+  getActivity(id: string): Promise<QuerySnapshot<unknown>> {
+    const homeTrainerRef = this.store.collection(FirebaseCollections.HOMETRAINER_ACTIVITY).ref;
+    const q = query(homeTrainerRef, where('idActivity', '==', true));
+    return getDocs(q);
+  }
+
 }
