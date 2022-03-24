46598962235df4387d9a9819a8087ef1e33035e0
sraye <sylvie.raye@budget-box.com>
2022-03-24 13:49:31 +0100

    Gp-35 Revoir couleurs et emplacement des notifications

--- src/app/services/notification.service.ts
+++ src/app/services/notification.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {MatSnackBar} from '@angular/material/snack-bar';
+import {MatSnackBar, MatSnackBarConfig} from '@angular/material/snack-bar';
 import {AlertComponent} from '../components/shared/alert/alert.component';
 import {AlertType} from '../models/alerts.model';
 
@@ -7,14 +7,22 @@ import {AlertType} from '../models/alerts.model';
   providedIn: 'root'
 })
 export class NotificationService {
+  alertConfig: MatSnackBarConfig = {
+    data: {},
+    duration: 6000,
+    horizontalPosition: 'center',
+    verticalPosition: 'bottom',
+  };
 
   constructor(private snackBar: MatSnackBar) { }
 
   success = (textToDisplay: string): void => {
-    this.snackBar.openFromComponent(AlertComponent, {data: {message: textToDisplay, alertType: AlertType.SUCCESS}});
+    this.alertConfig.data = {message: textToDisplay, alertType: AlertType.SUCCESS};
+    this.snackBar.openFromComponent(AlertComponent, this.alertConfig);
   }
 
   failure = (textToDisplay: string): void => {
-    this.snackBar.openFromComponent(AlertComponent, {data: {message: textToDisplay, alertType: AlertType.FAILURE}, duration: 6000});
+    this.alertConfig.data = {message: textToDisplay, alertType: AlertType.FAILURE};
+    this.snackBar.openFromComponent(AlertComponent, this.alertConfig);
   }
 }
